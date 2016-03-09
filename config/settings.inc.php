a3982704a4d42de55e01c7207f9f7695420f86bd
Jan Sanchez <joejansanchez@gmail.com>
2016-03-09 18:24:11 -0500

    setings

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'tienda2');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'rYzKoMIIxCuopC2F64L5G8iQ1fQmsAfWNI74GWuOacpDhEDeiGspZe81');
+define('_COOKIE_IV_', '9bmDbdCS');
+define('_PS_CREATION_DATE_', '2016-03-09');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.4');
+define('_RIJNDAEL_KEY_', 'SgykpsZv0BLtVpvCqxszmqaWwHuLdVdH');
+define('_RIJNDAEL_IV_', 'siCoYx9K0eR7EiBzM2WTMA==');
